6930a2c5acf7bf2dec4041fa458e16486876c5f4
lrizza <leonardorizza@hotmail.com>
2023-11-12 13:22:14 -0300

    Simutual prueba1

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
 abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
+    nav.style.display = 'flex';
 })
 
 cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
+    nav.style.display = 'none';
 })
